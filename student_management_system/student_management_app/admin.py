d2fe42ad106a5122037e843faabed6764022c611
Rishi1097 <rpoudyal8282@gmail.com>
2022-08-02 21:55:48 +0545

    Student_management

---
+++ student_management_system/student_management_app/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from student_management_app.models import CustomUser
+
+
+class UserModel(UserAdmin):
+    pass
+
+admin.site.register(CustomUser, UserModel)
+#admin.site.register(Staff)
+#admin.site.register(Student)
+#admin.site.register(Course)
+#admin.site.register(Subject)
+#admin.site.register(Session)
